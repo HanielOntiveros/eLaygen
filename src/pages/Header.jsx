d35bae9ee65d434fe3232d81565f3f3058e28830
HanielOnt <haniel@comprarsitioweb.com>
2024-11-11 21:42:31 -0600

    Replace test funcion on Header Section

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,10 +1,21 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { PageContext } from "../Context/PageContext"
 import { useForm } from "../hooks/useForm"
 
 export const Header = () => {
+    const [headingComponent, setHeadingComponent] = useState([])
     const { webData } = useContext(PageContext)
-    const { handleReset, handleSubmit } = useForm()
+    const { handleReset, handleSubmit } = useForm({ setHeadingComponent })
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        const component = e.target.value
+        setHeadingComponent(component.replace("${title}", "Soy un titulo cambiado por consola"))
+        // const formData = new FormData(form);
+        // const newData = [...formData].map(d => d[1])
+    }
+    console.log(headingComponent);
+
 
 
     return (
@@ -12,7 +23,7 @@ export const Header = () => {
             <h3>Header Section</h3>
             <form onSubmit={(e) => handleSubmit(e)} >
                 <div className="row jusitfy-content-start">
-                    <select name="components" id="pet-select" >
+                    <select name="components" id="pet-select" onChange={handleChange} >
                         <option value="">--Selecciona una opcion--</option>
                         {
                             webData.Header && webData.Header.map((item) => (
@@ -20,8 +31,9 @@ export const Header = () => {
                             ))
                         }
                     </select>
-
+                    {/* <button className='btn btn-outline-dark w-25 ' type="submit" onClick={handleChange}>Select</button> */}
                 </div>
+
                 <div className="my-3">
                     <button className='btn btn-outline-dark w-25 ' type="reset" onClick={(e) => handleReset(e)}>Reset</button>
 
